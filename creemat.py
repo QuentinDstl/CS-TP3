aecaded974cffcb0b102ce6d65a92ce67ada919a
Swordali23 <alireza.atefi@edu.ece.fr>
2022-10-11 11:34:09 +0200

    gaussseidel fini

--- creemat.py
+++ creemat.py
@@ -1,12 +1,15 @@
-from numpy import zeros,array
+from random import randint, random, randrange
+from numpy import zeros
 
 def creemat(n):
     A = zeros((n, n))
     b = zeros(n)
+    x0 = zeros(n)
     for i in range(n):
         A[i][i]=3
         if(i!=n-1):
             A[i+1][i]=1
             A[i][i+1]=1
         b[i]=1
-    return [A, b]
+        x0[i]=randint(0,10)
+    return [A, b, x0]
--- gaussseidel2.py
+++ gaussseidel2.py
@@ -1,40 +1,32 @@
 from decompDMN import decompDMN
 from trianginf import trianginf
 from creemat import creemat
-from numpy import zeros,array
+from numpy import zeros,array,dot
 
-[A,b] = creemat(5)
+[A,b,x0] = creemat(5)
 print(A)
 print(b)
+print(x0)
 
-# def gaussseidel(A, b, x0, eps, Nb):
-#     # A est la matrice
-#     # b est le vecteur second membre
-#     # x0 est la valeur initiale
-#     # eps est la précision
-#     # N est le nombre maximum d'itérations
+def gaussseidel(A, b, x0, eps, Nb):
+    # A est la matrice
+    # b est le vecteur second membre
+    # x0 est la valeur initiale
+    # eps est la précision
+    # N est le nombre maximum d'itérations
 
-#     [DM, N]= decompDMN(A)
-#     n = len(b)
-#     x = zeros(n)
-#     L = zeros(n)
+    [DM, N]= decompDMN(A)
+    n = len(b)
+    x = zeros(n)
+    L = zeros(n)
 
-#     for k in range(Nb):
-#         for i in range(0,n):
-#             S = 0
-#             for j in range(i+1,n):
-#                 S += N[i][j] * x0[j]
-#             L[i]=S+b[i]
-#         x=trianginf(DM,L)
-#         print(x)
-#         if(max(abs(x - x0)) < eps):
-#             return x
-#         x0 = x
-#     return x
+    for k in range(Nb):
+        L=dot(N,x0)+b
+        x=trianginf(DM,L)
+        if(max(abs(x - x0)) < eps):
+            return x
+        x0 = x
+    return x
 
-# x=gaussseidel(array([[1,1,-1],[2,-1,1],[-1,2,2]]), array([-2, 5, 1]),array([0, 0, 0]), 0.5,2)
-# print(x)
-
-# x=gaussseidel(array([[1,1,-1],[2,-1,1],[-1,2,2]]), array([-2, 5, 1]),array([0, 0, 0]), 0.5,2)
-# print(x)
-
+x=gaussseidel(A,b,x0, 0.001,100)
+print(x)
